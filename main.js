9769d5a4ca8b4d98d376088a4c52ad7cc87c969c
codeverb <codeverbonline@gmail.com>
2017-06-02 16:00:06 +0530

    ss

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+var express =require("express");
+var app = express();
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get("/",function(req,res){
+   res.send("welcome to my app"); 
+    
+    
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
